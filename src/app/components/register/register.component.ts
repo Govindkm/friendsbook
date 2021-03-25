4def341c20c12fdee294d853af39adefd0321929
Spectrex21 <43842811+Spectrex21@users.noreply.github.com>
2021-03-25 19:52:47 +0530

    Validated

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-
+const server_URL= "http://localhost:3000/users"
 function range(start, end) {
   var numbers=[];
 
@@ -22,12 +23,48 @@ export class RegisterComponent implements OnInit {
   days:number[];
   months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
   years:number[]
-  constructor() { 
+  emailList:Array<string>=[]
+
+  email: string;
+
+  notAvailable:string;
+
+  clicked:boolean = false;
+
+  constructor(private http:HttpClient) { 
     this.days=range(1,31);
     this.years=range(1900,2050);
   }
 
   ngOnInit(): void {
+
+    this.http.get<any>(server_URL).subscribe(response =>{
+      console.log(response)
+      response.forEach(y => {
+        
+        this.emailList.push(y.email)
+        
+      })
+      console.log(this.emailList)
+
+    })
   }
 
+  checkAvailablity(email){
+    this.clicked=true;
+    // console.log("this is the input")
+    console.log(email.value)
+    // console.log(this.emailList)
+    this.notAvailable=this.emailList.find(lid=>email.value === lid)
+    console.log(this.notAvailable)
+    
+    if (this.notAvailable){
+      return ("This is not available")
+      
+    }
+    else{
+      return ("Available")
+    }
+    
+  }
 }
